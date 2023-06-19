7fd439246dd078ee837fe66452afff459546632f
wasiq abdullah <chwasiq0569>
2023-06-19 18:49:14 +0500

    Extracting contours based on specific area

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('./images/scale.jpg')
+img = cv2.imread('./images/box.jpg')
 
 
 
@@ -27,11 +27,29 @@ while True:
     imgDilated = cv2.dilate(imgCanny, kernel, iterations=1)
 
     contours, hierarchy = cv2.findContours(imgDilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(imgContours, contours, -1, (255, 0, 255), 2)
+    # cv2.drawContours(imgContours, contours, -1, (255, 0, 255), 2)
+
+    min_area = 5000  # Minimum area threshold
+    filtered_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_area]
+
+    # Create a blank image to draw the contours
+    # Draw the filtered contours as a complete outline
+    cv2.drawContours(imgContours, filtered_contours, -1, (0, 0, 0), 2)
+
+    # Display the image with contours
+    cv2.imshow("Image with Contours", imgContours)
+
+    # for cnt in contours:
+    #     area = cv2.contourArea(cnt)
+    #     if area > 28000:
+    #         cv2.drawContours(imgContours, cnt, -1, (0, 0, 0), 2)
+
 
     cv2.imshow("Image", imgContours)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+
 # imgBlur = cv2.GaussianBlur(img, (3, 3), 1)
 
 # imgContours = img.copy()
