4c17182b79c5b2f32673172ed775abf61b1b5198
wasiq abdullah <chwasiq0569>
2023-06-19 13:04:39 +0500

    Applied Canny Edge Detection

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('./box.jpg')
+img = cv2.imread('./images/card333.jpg')
 
 
 
@@ -13,14 +13,17 @@ cv2.resizeWindow("Parameters", 640, 240)
 cv2.createTrackbar("Threshold1", "Parameters", 150, 254, empty)
 cv2.createTrackbar("Threshold2", "Parameters", 254, 254, empty)
 #
-# while True:
-#     imgBlur = cv2.GaussianBlur(img, (3, 3), 1)
-#
-#     threshold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
-#     threshold2 = cv2.getTrackbarPos("Threshold2", "Parameters")
-imgBlur = cv2.GaussianBlur(img, (0, 0), 1)
+while True:
+    imgBlur = cv2.GaussianBlur(img, (3, 3), 1)
 
-cv2.imshow("Image", img)
+    threshold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
+    threshold2 = cv2.getTrackbarPos("Threshold2", "Parameters")
+
+    imgCanny = cv2.Canny(imgBlur, threshold1, threshold2)
+    print(imgCanny)
+    cv2.imshow("Image", imgCanny)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
