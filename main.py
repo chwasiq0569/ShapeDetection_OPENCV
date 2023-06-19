1eed55c930ffcb0584aa74f56fd01bdb4baea0e5
wasiq abdullah <chwasiq0569>
2023-06-19 18:57:33 +0500

    Added Tackbar for Area

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ cv2.namedWindow('Parameters')
 cv2.resizeWindow("Parameters", 640, 240)
 cv2.createTrackbar("Threshold1", "Parameters", 150, 254, empty)
 cv2.createTrackbar("Threshold2", "Parameters", 254, 254, empty)
+cv2.createTrackbar("Area", "Parameters", 0, 35000, empty)
+
+
 #
 while True:
     imgBlur = cv2.GaussianBlur(img, (3, 3), 1)
@@ -19,6 +22,7 @@ while True:
 
     threshold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
     threshold2 = cv2.getTrackbarPos("Threshold2", "Parameters")
+    areaMin = cv2.getTrackbarPos("Area", "Parameters")
 
     imgCanny = cv2.Canny(imgBlur, threshold1, threshold2)
 
@@ -29,8 +33,8 @@ while True:
     contours, hierarchy = cv2.findContours(imgDilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     # cv2.drawContours(imgContours, contours, -1, (255, 0, 255), 2)
 
-    min_area = 5000  # Minimum area threshold
-    filtered_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_area]
+    # min_area = 5000  # Minimum area threshold
+    filtered_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > areaMin]
 
     # Create a blank image to draw the contours
     # Draw the filtered contours as a complete outline
@@ -45,26 +49,9 @@ while True:
     #         cv2.drawContours(imgContours, cnt, -1, (0, 0, 0), 2)
 
 
-    cv2.imshow("Image", imgContours)
+    cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-
-# imgBlur = cv2.GaussianBlur(img, (3, 3), 1)
-
-# imgContours = img.copy()
-
-# imgCanny = cv2.Canny(imgBlur, 150, 254)
-
-# kernel = np.ones((1, 1), np.uint8)
-
-# imgDilated = cv2.dilate(imgCanny, kernel, iterations=1)
-
-# contours, hierarchy = cv2.findContours(imgDilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-# cv2.drawContours(imgContours, contours, -1, (255, 0, 255), 7)
-
-# cv2.imshow("Image", imgContours)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
